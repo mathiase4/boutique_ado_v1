33a51454c50d52303bab069d0312fa9f30b301e7
mathiase4 <mathiaseriksson98@hotmail.com>
2025-10-14 17:20:52 +0200

    Completed webhook handler

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
 from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
@@ -10,20 +10,22 @@ from bag.contexts import bag_contents
 
 import stripe
 import json
+
+
 @require_POST
 def cache_checkout_data(request):
     try:
         pid = request.POST.get('client_secret').split('_secret')[0]
         stripe.api_key = settings.STRIPE_SECRET_KEY
         stripe.PaymentIntent.modify(pid, metadata={
-            'bag': json_dumps(request.session.get('bag', {})),
-            'save.info': request.POST.get('save_info'),
+            'bag': json.dumps(request.session.get('bag', {})),
+            'save_info': request.POST.get('save_info'),
             'username': request.user,
         })
         return HttpResponse(status=200)
     except Exception as e:
-        messages.error(request,'Sorry, your payment cannot be \
-            processed right now. Please try again later.' )
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
         return HttpResponse(content=e, status=400)
 
 
@@ -47,7 +49,11 @@ def checkout(request):
         }
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
             for item_id, item_data in bag.items():
                 try:
                     product = Product.objects.get(id=item_id)
@@ -80,7 +86,6 @@ def checkout(request):
         else:
             messages.error(request, 'There was an error with your form. \
                 Please double check your information.')
-
     else:
         bag = request.session.get('bag', {})
         if not bag:
@@ -98,7 +103,8 @@ def checkout(request):
 
         order_form = OrderForm()
 
-
+        # in the video, the below code is not indented properly
+        # this is the correct indentation
         if not stripe_public_key:
             messages.warning(request, 'Stripe public key is missing. \
                 Did you forget to set it in your environment?')
@@ -111,7 +117,7 @@ def checkout(request):
         }
 
         return render(request, template, context)
-
+        # end of the corrected indentation
 
 
 def checkout_success(request, order_number):
@@ -123,7 +129,7 @@ def checkout_success(request, order_number):
     messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. A confirmation \
         email will be sent to {order.email}.')
-    
+
     if 'bag' in request.session:
         del request.session['bag']
 
@@ -131,4 +137,5 @@ def checkout_success(request, order_number):
     context = {
         'order': order,
     }
+
     return render(request, template, context)
